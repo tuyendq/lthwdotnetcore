e6885ed360379d82c8a36ca077dc626691e7962e
Tuyen Dang <tuyend@gmail.com>
2018-03-01 22:55:24 +0700

    Add Fibonacci folder

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Why don't you write the Fibonacci yet? Try it!");
+            }
+
+            Console.WriteLine("Fibonacci Numbers 1-15:");
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"{i + 1}: {FibonacciNubmer(i)}");
+            }            
+        }
+
+        static int FibonacciNumber(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int tmp;
+
+            for (int i = 0; i < n; i++)
+            {
+                tmp = a;
+                a = b;
+                b += tmp;
+            }
+
+            return a;
+        }
+
+    }
+}
