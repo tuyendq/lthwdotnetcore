7ace4f41a4297e74ae10eebcf089cdae51e77d7e
Tuyen Dang <tuyend@gmail.com>
2018-03-31 17:44:34 +0700

    Add ConsoleApp.SQLite

---
+++ ConsoleApp.SQLite/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp.SQLite
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Getting Started with EF Core on .NET Core Console App with a New database!");
+
+            using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet"});
+
+                db.Blogs.Add(new Blog { Url = "http://www.practicehabits.net" });
+
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in databse:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(" - {0} ", blog.Url);
+                }
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp.SQLite/README.md
@@ -0,0 +1,2 @@
+# .NET Core EF
+# https://docs.microsoft.com/en-us/ef/core/get-started/netcore/new-db-sqlite
